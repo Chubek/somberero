7ebe22c3a0d632a5b8ae9aca85069363a2946e60
Chubak Bidpaa <chubakbidpaa@gmail.com>
2022-10-01 03:10:12 +0330

    added cookies

--- stress_test.py
+++ stress_test.py
@@ -3,11 +3,16 @@ from dotenv import load_dotenv
 import os
 from search_google import search_google
 import time
+import json
 
 load_dotenv()
 
 REMOTE_URL = os.environ['REMOTE_URL']
 SC_FOLDER = os.environ['SC_FOLDER']
+
+with open("./www.linkedin.com.cookies.json", "r") as fc:
+    contents = fc.read()
+    cookies_dict = json.loads(contents)
 
 if not os.path.exists(SC_FOLDER):
     os.makedirs(SC_FOLDER)
@@ -18,6 +23,10 @@ driver = webdriver.Remote(
     options=firefox_options
 )
 
+driver.get("https://www.linkedin.com/")
+
+for cookie in cookies_dict:
+    driver.add_cookie({"name": cookie['name'], "value": cookie['value'], "domain": cookie['domain']})
 
 search_queries = [
     "marketing united kingdom site:linkedin.com/in/",
